26da5dbc5507cf2b19615142cd5664a4a538c857
Scott Wu <sw@scottwu.ca>
2025-05-11 18:06:52 +0800

    one line

--- src/routes/docs/+page.ts
+++ src/routes/docs/+page.ts
@@ -4,11 +4,9 @@ import type { DocFile } from '$lib/types/docs.js'
 
 export const load: PageLoad = async () => {
 	// @ts-expect-error import doesn't recognize *.md modules _yet_
-	const doc: DocFile = await import('../../content/index.md')
 
-	if (!doc || !doc.metadata) {
-		error(404)
-	}
+	if (!doc || !doc.metadata) error(404)
+
 	return {
 		doc: doc.default,
 		metadata: doc.metadata,
--- src/routes/docs/[...slug]/+page.ts
+++ src/routes/docs/[...slug]/+page.ts
@@ -5,9 +5,8 @@ import { getDoc } from '$lib/utils.js'
 
 export const load: PageLoad = async (event) => {
 	const doc: DocFile = await getDoc(event.params.slug)
-	if (!doc || !doc.metadata) {
-		error(404)
-	}
+	if (!doc || !doc.metadata) error(404)
+
 	return {
 		doc: doc.default,
 		metadata: doc.metadata,
