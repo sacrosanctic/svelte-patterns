da8640ca164ae7e8b9969ccc8d48343c12e330ef
Scott Wu <sw@scottwu.ca>
2025-05-11 18:13:47 +0800

    hide redundant title

--- src/lib/components/document/doc-header.svelte
+++ src/lib/components/document/doc-header.svelte
@@ -1,11 +1,9 @@
 <script>
-	import { page } from '$app/state'
-
 	let { title, description } = $props()
 </script>
 
 <header class="mb-4 border-b border-gray-200 pb-2 dark:border-neutral-700 sm:mb-4 sm:pb-6">
-	{#if page.params.slug}
+	<!-- {#if page.params.slug}
 		<p class="mb-2 text-sm font-semibold capitalize text-primary">
 			{page.params.slug
 				.replace(/-/g, ' ')
@@ -14,7 +12,7 @@
 		</p>
 	{:else}
 		<p class="mb-2 text-sm font-semibold capitalize text-primary">Docs</p>
-	{/if}
+	{/if} -->
 	<h1 class="mb-2 block text-2xl font-bold sm:text-3xl">
 		{title}
 	</h1>
