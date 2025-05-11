ea41334722b5205d7939b5076d9c39c7410a7e35
Scott Wu <sw@scottwu.ca>
2025-05-11 18:56:30 +0800

    put import back

--- src/routes/docs/+page.ts
+++ src/routes/docs/+page.ts
@@ -4,6 +4,7 @@ import type { DocFile } from '$lib/types/docs.js'
 
 export const load: PageLoad = async () => {
 	// @ts-expect-error import doesn't recognize *.md modules _yet_
+	const doc: DocFile = await import('../../content/index.md')
 
 	if (!doc || !doc.metadata) error(404)
 
