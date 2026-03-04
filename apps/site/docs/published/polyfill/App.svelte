54fc4884c10be46be16f3752d72007768b5efb8f
Scott Wu <sw@scottwu.ca>
2026-03-04 23:51:51 +0800

    add polyfill

---
+++ apps/site/docs/published/polyfill/App.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Polyfill from './Polyfill.svelte'
+</script>
+
+<Polyfill src="https://cdn.jsdelivr.net/npm/temporal-polyfill@0.3.0/global.min.js">
+	{Temporal.Now.zonedDateTimeISO().toString()}
+</Polyfill>
