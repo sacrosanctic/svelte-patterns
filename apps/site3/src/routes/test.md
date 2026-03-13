8091f64405d5ed816504b9ea4e898d4c60d0d907
Scott Wu <sw@scottwu.ca>
2026-03-13 16:12:34 +0800

    add markdown-it

---
+++ apps/site3/src/routes/test.md
@@ -0,0 +1,15 @@
+---
+title: Markdown to Svelte
+---
+
+# Convert Markdown to Svelte Component
+
+- List
+- List
+- List
+
+<script>
+    import MyComponent from './MyComponent.svelte'
+</script>
+
+<MyComponent>You can use Svelte components in Markdown</MyComponent>
--- apps/site3/svelte.config.js
+++ apps/site3/svelte.config.js
@@ -1,6 +1,9 @@
 import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+	extensions: ['.svelte', '.md'],
+	kit: { adapter: adapter() }
+};
 
 export default config;
