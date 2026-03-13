14017a1bf9ffe48212aa3b92f70ba11454094dc4
Scott Wu <sw@scottwu.ca>
2026-03-13 13:04:39 +0800

    add test file

--- packages/ui/src/routes/+page.svelte
+++ packages/ui/src/routes/+page.svelte
@@ -1,3 +1,23 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { SvelteRepl, SveltelabRepl } from '$lib/index.js'
+	import appFile from './App.svelte?raw'
+	import counterFile from './Counter.svelte?raw'
+
+	const svelteFiles = [
+		{ name: 'App.svelte', contents: appFile },
+		{ name: 'Counter.svelte', contents: counterFile },
+	]
+
+	const sveltelabFiles = [
+		{ name: 'src/routes/+page.svelte', contents: appFile },
+		{ name: 'src/routes/Counter.svelte', contents: counterFile },
+	]
+</script>
+
+<h1>REPL Demos</h1>
+
+<h2>Svelte.dev Playground</h2>
+<SvelteRepl name="test" files={svelteFiles} />
+
+<h2>Sveltelab Playground</h2>
+<SveltelabRepl files={sveltelabFiles} />
