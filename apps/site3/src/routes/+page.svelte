45f327b0ab23ff00f52fd4b7705ed95a3a6c6f28
Scott Wu <sw@scottwu.ca>
2026-03-13 13:44:48 +0800

    .

--- apps/site3/src/routes/+page.svelte
+++ apps/site3/src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { SvelteRepl, SveltelabRepl } from '@repo/ui';
+	import appFile from './App.svelte?raw';
+	import counterFile from './Counter.svelte?raw';
+
+	const svelteFiles = [
+		{ name: 'App.svelte', contents: appFile },
+		{ name: 'Counter.svelte', contents: counterFile }
+	];
+
+	const sveltelabFiles = [
+		{ name: 'src/routes/+page.svelte', contents: appFile },
+		{ name: 'src/routes/Counter.svelte', contents: counterFile }
+	];
+</script>
+
+<h1>REPL Demos</h1>
+
+<h2>Svelte.dev Playground</h2>
+<SvelteRepl name="test" files={svelteFiles} />
+
+<h2>Sveltelab Playground</h2>
+<SveltelabRepl files={sveltelabFiles} />
