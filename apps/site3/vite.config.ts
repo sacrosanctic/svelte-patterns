69258de4a604d465efc99fe395506ba4ffe2125c
Scott Wu <sw@scottwu.ca>
2026-03-17 11:11:56 +0800

    .

--- apps/site3/vite.config.ts
+++ apps/site3/vite.config.ts
@@ -1,34 +1,46 @@
-import devtoolsJson from 'vite-plugin-devtools-json';
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-import svelteMd from 'vite-plugin-svelte-md';
-import { snippet } from '@mdit/plugin-snippet';
-import { join, resolve } from 'node:path';
+import devtoolsJson from 'vite-plugin-devtools-json'
+import tailwindcss from '@tailwindcss/vite'
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vite'
+import svelteMd from 'vite-plugin-svelte-md'
+import { snippet } from '@mdit/plugin-snippet'
+import { container } from '@mdit/plugin-container'
+import { join, resolve } from 'node:path'
 
-const rootPath = resolve(__dirname);
+const rootPath = resolve(__dirname)
 
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
 		svelteMd({
 			markdownItOptions: {},
-			use: (md) =>
-				// @ts-expect-error plugin doesnt support ts (probably)
+			use: (md) => {
+				// @ts-expect-error https://github.com/serkodev/markdown-exit/issues/30
+				// type incompatibility with markdown-it and markdown-exit
+				md.use(container, {
+					name: 'code-group',
+					// marker: ':',
+					openRender: () => `<div class="code-group">\n`,
+					closeRender: () => `</div>\n`,
+				})
+				// @ts-expect-error https://github.com/serkodev/markdown-exit/issues/30
+				// type incompatibility with markdown-it and markdown-exit
 				md.use(snippet, {
 					currentPath: (env) => env.id,
 					resolvePath: (filePath) => {
 						if (filePath.startsWith('/')) {
-							return join(rootPath, filePath.slice(1));
+							return join(rootPath, filePath.slice(1))
 						}
 						if (filePath.startsWith('$lib/')) {
-							return join(rootPath, 'src/lib', filePath.slice(5));
+							return join(rootPath, 'src/lib', filePath.slice(5))
 						}
-						return filePath;
-					}
+						return filePath
+					},
 				})
+				return md
+			},
 		}),
 		sveltekit(),
-		devtoolsJson()
-	]
-});
+		devtoolsJson(),
+	],
+})
