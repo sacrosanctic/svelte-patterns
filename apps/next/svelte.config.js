5f0c5c28c734f3c1131eac9abc3c69711de9c8c8
Scott Wu <sw@scottwu.ca>
2026-04-16 15:10:26 +0800

    disable prerender

--- apps/next/svelte.config.js
+++ apps/next/svelte.config.js
@@ -1,13 +1,20 @@
 import adapternode from '@sveltejs/adapter-node'
 import adapterVercel from '@sveltejs/adapter-vercel'
 
-const isDev = process.env.NODE_ENV === 'development'
+const isDev = process.env.VERCEL_ENV !== 'preview' || process.env.VERCEL_ENV !== 'production'
 const adapter = isDev ? adapternode : adapterVercel
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', '.md'],
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter(),
+		prerender: {
+			handleMissingId: (details) => {
+				if (details.id === 'main-content') return
+			},
+		},
+	},
 }
 
 export default config
